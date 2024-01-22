23b8c2575ed2b1448a69a7f277f0d4ca724ef3ad
raudis <rconsoro24@gmail.com>
2024-01-22 18:27:49 +0100

    Portafolio pagina principal finalizada

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,31 @@
+import linkedin from "./img/linkedin.webp";
+import git from "./img/git.png";
+import gmail from "./img/gmail.jpg";
+import corazon from "./img/corazon.png";
 const Footer = () => {
   return (
-    <footer className="p-4 text-center">
-      <p className="text-2xl">Todos Los Derechos Reservados 2024</p>
+    <footer className="p-4 text-center flex justify-center gap-8">
+      <p className="md:text-2xl">
+        <img
+          src={corazon}
+          alt="icono de corazon"
+          width={50}
+          height={50}
+          className="inline-block mr-2 "
+        />
+        Todos Los Derechos Reservados Raudis Consoro ©2024
+      </p>
+      <div className="flex  justify-around gap-4">
+        <a href="#">
+          <img width={30} height={30} src={gmail} alt="icono de gmail" />
+        </a>
+        <a href="#">
+          <img width={30} height={30} src={git} alt="icono de git" />
+        </a>
+        <a href="#">
+          <img width={30} height={30} src={linkedin} alt="icono de linkedin" />
+        </a>
+      </div>
     </footer>
   );
 };
